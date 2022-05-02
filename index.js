898ef514a3590ff587caf9929a1151e1179cd61f
Lucas Castro <professorlucascastro@gmail.com>
2022-05-02 12:07:46 -0300

    Criando projeto

---
+++ index.js
@@ -0,0 +1,38 @@
+/* Conversor de números inteiros para ROMANOS 
+
+Os algarismos romanos são representados por sete símbolos diferentes:  I, V, X, L, C, De M.
+
+Valor do símbolo
+eu 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+Por exemplo,  2é escrito II em numeral romano, apenas dois um são somados. 12é escrito como  XII, que é simplesmente X + II. O número 27é escrito como XXVII, que é XX + V + II.
+
+Os algarismos romanos são geralmente escritos do maior para o menor da esquerda para a direita. No entanto, o numeral para quatro não é IIII. Em vez disso, o número quatro é escrito como IV. Porque o um está antes do cinco, nós o subtraímos fazendo quatro. O mesmo princípio se aplica ao número nove, que é escrito como IX. Há seis casos em que a subtração é usada:
+
+Ipode ser colocado antes Vde (5) e X(10) para fazer 4 e 9. 
+Xpode ser colocado antes de L(50) e C(100) para fazer 40 e 90. 
+Cpode ser colocado antes D(500) e M(1000) para fazer 400 e 900.
+Dado um número inteiro, converta-o para um numeral romano.
+
+ 
+
+Exemplo 1:
+
+Entrada: num = 3
+ Saída: "III"
+ Explicação: 3 é representado como 3 unidades.
+Exemplo 2:
+
+Entrada: num = 58
+ Saída: "LVIII"
+ Explicação: L = 50, V = 5, III = 3.
+Exemplo 3:
+
+Entrada: num = 1994
+ Saída: "MCMXCIV"
+ Explicação: M = 1000, CM = 900, XC = 90 e IV = 4. */
